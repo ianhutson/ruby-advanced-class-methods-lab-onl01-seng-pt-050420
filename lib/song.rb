9ccaf0f24626db8e23086edd5d8bec4062a69c1e
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-26 17:20:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,5 +34,9 @@ class Song
   
   def self.find_or_create_by_name(name)
    self.find_by_name(name) || self.create_by_name(name)
- end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by()
+  end
 end
