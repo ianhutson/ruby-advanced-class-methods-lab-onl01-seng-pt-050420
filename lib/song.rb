0e1ef8a7fcde2c107236b5c31d6155e7af722f31
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-26 17:19:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,10 +33,5 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if @@all.include? self.name
-      self.find_by_name(name)
-    else 
-     self.create_by_name(name)
-    end
-  end
+   self.find_by_name(song_name) || self.create_by_name(song_name)
 end
